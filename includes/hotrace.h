c0c4b38667067c9ec9f3116f421760252f9aa15a
Cedric POULET <cpoulet@student.42.fr>
2017-05-27 11:29:30 +0200

    Structure general

---
+++ includes/hotrace.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hotrace.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpoulet <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/27 11:19:57 by cpoulet           #+#    #+#             */
+/*   Updated: 2017/05/27 11:27:52 by cpoulet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HOTRACE_H
+# define HOTRACE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+int	ft_getline(const int fd, char **line);
+
+#endif
